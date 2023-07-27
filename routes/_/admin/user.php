beeb600fd211bca313f5f7b545a6a7085546d362
Aria Agus Riadi <ariaagusriadi100@gmail.com>
2023-07-28 00:20:54 +0700

    userController

--- routes/_/admin/user.php
+++ routes/_/admin/user.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('user', [UserController::class, 'index']);
