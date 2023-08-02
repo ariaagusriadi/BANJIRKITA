722c5e8eace47d1e2546346f529d13fdc5b3731f
Aria Agus Riadi <ariaagusriadi100@gmail.com>
2023-08-02 16:04:28 +0700

    update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -6,12 +6,12 @@
                     <i class="ti ti-menu-2"></i>
                 </a>
             </li>
-            <li class="nav-item d-none d-lg-block">
+            {{-- <li class="nav-item d-none d-lg-block">
                 <a class="nav-link nav-icon-hover" href="javascript:void(0)" data-bs-toggle="modal"
                     data-bs-target="#exampleModal">
                     <i class="ti ti-search"></i>
                 </a>
-            </li>
+            </li> --}}
         </ul>
 
         <div class="d-block d-lg-none">
