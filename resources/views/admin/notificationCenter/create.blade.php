c061143a848ed0861e84fabac388074233d0dc4f
Aria Agus Riadi <ariaagusriadi100@gmail.com>
2023-08-08 00:13:15 +0700

    update

--- resources/views/admin/notificationCenter/create.blade.php
+++ resources/views/admin/notificationCenter/create.blade.php
@@ -182,15 +182,15 @@
             $('#desc').summernote({
                 tabsize: 2,
                 height: 150,
-                toolbar: [
-                    // [groupName, [list of button]]
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['font', ['strikethrough', 'superscript', 'subscript']],
-                    ['fontsize', ['fontsize']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['height', ['height']]
-                ]
+                // toolbar: [
+                //     // [groupName, [list of button]]
+                //     ['style', ['bold', 'italic', 'underline', 'clear']],
+                //     ['font', ['strikethrough', 'superscript', 'subscript']],
+                //     ['fontsize', ['fontsize']],
+                //     ['color', ['color']],
+                //     ['para', ['ul', 'ol', 'paragraph']],
+                //     ['height', ['height']]
+                // ]
             });
             $('#location').on('change', function() {
                 let id = $(this).val();
